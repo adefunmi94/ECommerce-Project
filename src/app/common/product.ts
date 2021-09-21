a7f97a278a223509c2c28cd78e746d76cd1f25af
Funmilayo Oyeyipo <foyeyipo@OLDMUTUALNIGERIA.COM>
2021-09-21 16:36:02 +0100

    Verion 1.0- first Commit

---
+++ src/app/common/product.ts
@@ -0,0 +1,15 @@
+export class Product {
+
+   sku!: string;
+
+    name!: string;
+    description!: string;
+    unitPrice!: number;
+    imageUrl!: String;
+    active!: boolean;
+    unitsInStock!: number;
+    dateCreated!: Date;
+    lastUpdated!: Date;
+
+
+}
