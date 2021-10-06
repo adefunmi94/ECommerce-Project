45fa33ac9724997f5e9d9627ac302b2bcbbf142c
Funmilayo Oyeyipo <foyeyipo@OLDMUTUALNIGERIA.COM>
2021-10-06 23:41:05 +0100

    Pagination

--- src/main/java/com/personalproject/ecommerce/config/DataRestConfig.java
+++ src/main/java/com/personalproject/ecommerce/config/DataRestConfig.java
@@ -2,15 +2,28 @@ package com.personalproject.ecommerce.config;
 
 import com.personalproject.ecommerce.entity.Product;
 import com.personalproject.ecommerce.entity.ProductCategory;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+    
+    private EntityManager theEntitymanager;
+
+    @Autowired
+    public DataRestConfig(EntityManager theEntitymanager) {
+        this.theEntitymanager = theEntitymanager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -26,5 +39,30 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsurportedMethod));
 
+//        call internal helper method
+
+        exposedIds(config);
+
+    }
+
+    private void exposedIds(RepositoryRestConfiguration config) {
+//        exposed entity ids
+
+//        get a list of entity class from entity Manager
+
+        Set<EntityType<?>> entityTypeSet = theEntitymanager.getMetamodel().getEntities();
+
+//        create an Array of an entity class
+        List<Class>  entityClasses = new ArrayList<>();
+
+//        - get the entity type from the entities
+
+        for (EntityType tempEntityType : entityTypeSet){
+            entityClasses.add(tempEntityType.getJavaType());
+
+        }
+//        exposed the entity ids for the array of entity/domain type
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
--- src/main/java/com/personalproject/ecommerce/dao/ProductRepo.java
+++ src/main/java/com/personalproject/ecommerce/dao/ProductRepo.java
@@ -1,11 +1,20 @@
 package com.personalproject.ecommerce.dao;
 
 import com.personalproject.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepo extends JpaRepository<Product, Long> {
+
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
+
 }
